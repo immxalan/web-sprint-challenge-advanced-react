68f6c26ac1c0ea22a26468e4e9aaecb87ec06c92
Allen Do <57642471+immxalan@users.noreply.github.com>
2020-07-18 11:02:48 -0700

    Stretch goal completed

--- client/src/components/ShoppingCart.js
+++ client/src/components/ShoppingCart.js
@@ -5,7 +5,7 @@ import React from "react";
 export default function ShoppingCart(props) {
   const total = props.cart.reduce((sum, d) => sum + d.price, 0);
   return (
-    <div className="cart">
+    <div className="cart" data-testid="cart">
       {props.cart.map((plant) => (
         <div className="plant-card" key={plant.id}>
           <img className="plant-image" src={plant.img} alt={plant.name} />
@@ -15,6 +15,7 @@ export default function ShoppingCart(props) {
             <button
               className="plant-button"
               onClick={() => props.removeFromCart(plant)}
+              data-testid="remove"
             >
               Remove
             </button>
---
+++ client/src/components/ShoppingCart.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import ShoppingCart from "./ShoppingCart";
+
+const plants = [{
+    name: "Snake Plant",
+    id: 4893,
+    scientificName: "Sansevieria zeylanica",
+    difficulty: "easy",
+    light: "direct",
+    img:
+      "https://cdn.shopify.com/s/files/1/2781/9558/products/6__SANSEVIERIA_ZEYLANICA-1_800x.png?v=1587146468",
+    sizes: ["small", "medium"],
+    watering: 2,
+    description:
+      "One of the most popular and hardy of houseplants, he's virtually indestructible and adaptable to almost any condition. Whether you throw full, direct sunlight at him or shove him in the low-light corner of your apartment, he'll grow. And to top it off, he'll go weeks without water if he must.",
+    price: 18,
+  }]
+
+test('remove button removes item from cart', () => {
+	const { getByTestId } = render(<ShoppingCart cart={plants} />);
+	const cart = getByTestId('cart');
+	// const button = getByTestId('remove');
+	// fireEvent.click(button);
+	expect(cart).toHaveTextContent(/snake plant/i);
+});
